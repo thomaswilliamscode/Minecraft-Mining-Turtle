a67d3e3ad34dd3edff311fee30ba603c7500dd2e
thomaswilliamscode <64794727+thomaswilliamscode@users.noreply.github.com>
2022-08-26 17:30:29 -0400

    Added Comments To help Understand what is going on.

--- Turtle.lua
+++ Turtle.lua
@@ -1,20 +1,26 @@
 -- Turtle Object --
 
-Turtle = {  minFuelLevel = 20000, 
+Turtle = {  -- variables -- 
+            minFuelLevel = 20000, 
             fuelLevel = 0,
 
+      -- functions -- 
+
+      -- This returns the fuel level that we store in the object variable -- 
       getFuelLevel = function()
         return Turtle.fuelLevel  
         end,
       
+      -- This sets object fuel level to the correct value -- 
       setFuelLevel = function()
         Turtle.fuelLevel = turtle.getFuelLevel()
         end,
 
+      -- Fuel check must pass to continue -- 
       startingFuel = function()
           Turtle.setFuelLevel()
           if Turtle.fuelLevel < Turtle.minFuelLevel then
-      
+             
 } 
 
 -- Check Fuel lvl -- 
